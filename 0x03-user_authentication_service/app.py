f2c8e78dfc4c503e289ec3e61891238e60b5e8da
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-11-22 18:08:56 +0000

    Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,7 +96,7 @@ def get_reset_password_token() -> str:
     })
 
 
-@app.route("/update_password", methods=["PUT"], strict_slashes=False)
+@app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """PUT /reset_password
 
