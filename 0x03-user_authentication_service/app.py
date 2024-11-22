74424592e628a113f04f3ee2a8b7e3b50aec4961
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-11-22 15:08:51 +0000

    import redirect

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,8 @@
 """
 basic Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
+
 from auth import Auth
 
 app = Flask(__name__)
