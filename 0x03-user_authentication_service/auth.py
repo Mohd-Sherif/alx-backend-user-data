40e55be4554fc70a457c65923ec5b310fa303f6f
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-11-22 14:53:06 +0000

    Destroy session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,11 @@ class Auth:
         except NoResultFound:
             return None
         return user
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Destroy Session
+        """
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
